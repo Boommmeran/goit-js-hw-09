d44c2f6d0de175e8d632d3a64fb879743ba47581
Mykhailo Volontyr <boommmeran@gmail.com>
2023-01-12 12:53:29 +0200

    add color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let intervalId = null;
 
+
+startButtonEl.addEventListener('click', onStartClick);
+stopButtonEl.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  if (intervalId === 1) {
+    return;
+  };
+  intervalId = setInterval(bgColorSwither, 1000);
+};
+
+function onStopClick() {
+  clearInterval(intervalId);
+};
+
+function bgColorSwither() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
